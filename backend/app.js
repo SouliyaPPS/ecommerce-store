7c8e405c23b6ceee01d6d8ebdb0e8d00f915c1bb
SouliyaPPS <souliyappsdev@gmail.com>
2022-02-04 18:38:23 +0700

    store

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// require the product route
+const product = require("./routes/productRoute");
+
+app.use("/api/v1", product);
+
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const dotenv = require("dotenv");
 
 // configure the dotenv variable
-dotenv.config({path: "./config/config.env"});
+dotenv.config({path: "backend/config/config.env"});
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
