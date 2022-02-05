21b1af7eda015feaa1960805baa83026f1b112a6
Souliya PPS <souliyappsdev@gmail.com>
2022-02-05 12:45:18 +0700

    Update

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,6 +10,34 @@ exports.createProduct = async (req, res, next) => {
   })
 }
 
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: 'Route is Work Find' })
+// get all products from the rest api
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find()
+  res.status(200).json({
+    success: true,
+    products,
+  })
+}
+
+// update a product by id
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: 'Product not found',
+    })
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  })
+
+  res.status(200).json({
+    success: true,
+    product,
+  })
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,14 @@
 // routes for all product controllers
-const express = require("express");
-const { getAllProducts, createProduct } = require("../controllers/productController");
-const router = express.Router();
+const express = require('express')
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+} = require('../controllers/productController')
+const router = express.Router()
 
-router.route("/products").get(getAllProducts);
-router.route("/product/new").post(createProduct);
+router.route('/products').get(getAllProducts)
+router.route('/product/new').post(createProduct)
+router.route('/product/:id').put(updateProduct)
 
-module.exports = router;
+module.exports = router
